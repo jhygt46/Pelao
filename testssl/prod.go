5dd43085a6e7b41c2103835df28204a75eebd112
jhygt46 <misitiodelivery@gmail.com>
2022-12-12 17:08:54 -0300

    aa

--- testssl/prod.go
+++ testssl/prod.go
@@ -1,33 +1,110 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
-	"strings"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
 
 	"github.com/valyala/fasthttp"
 )
 
-type MyHandler struct{}
-
-func (h *MyHandler) requestHandler(ctx *fasthttp.RequestCtx) {
-	if string(ctx.Method()) == "GET" {
-		switch string(ctx.Path()) {
-		case "/init":
-			ctx.Response.Header.Set("Content-Type", "application/json")
-			var b strings.Builder
-			b.Grow(10)
-			b.Write([]byte{72, 79, 76, 65, 32, 77, 85, 78, 68, 79})
-			fmt.Println("SERVER 1")
-			fmt.Fprint(ctx, b.String())
-		default:
-			ctx.Error("Not Found", fasthttp.StatusNotFound)
-		}
-	}
+type Config struct {
+	Tiempo time.Duration `json:"Tiempo"`
+}
+type MyHandler struct {
+	Conf  Config `json:"Conf"`
+	Count uint64 `json:"Count"`
 }
 
 func main() {
 
-	pass := &MyHandler{}
-	fasthttp.ListenAndServe("80", pass.requestHandler)
+	var port string
+	if runtime.GOOS == "windows" {
+		port = ":81"
+	} else {
+		port = ":8080"
+	}
 
+	pass := &MyHandler{
+		Conf: Config{},
+	}
+
+	con := context.Background()
+	con, cancel := context.WithCancel(con)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGHUP)
+	defer func() {
+		signal.Stop(signalChan)
+		cancel()
+	}()
+	go func() {
+		for {
+			select {
+			case s := <-signalChan:
+				switch s {
+				case syscall.SIGHUP:
+					pass.Conf.init()
+				case os.Interrupt:
+					cancel()
+					os.Exit(1)
+				}
+			case <-con.Done():
+				log.Printf("Done.")
+				os.Exit(1)
+			}
+		}
+	}()
+	go func() {
+		fasthttp.ListenAndServe(port, pass.HandleFastHTTP)
+	}()
+	if err := run(con, pass, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+
+}
+
+func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
+
+	if string(ctx.Method()) == "GET" {
+		switch string(ctx.Path()) {
+		case "/":
+			h.Count++
+			fmt.Fprintf(ctx, "HOLA")
+		case "/stats":
+			fmt.Fprintf(ctx, "Count %v", h.Count)
+		default:
+			ctx.Error("Not Found", fasthttp.StatusNotFound)
+		}
+	}
+
+}
+
+// DAEMON //
+func (h *MyHandler) StartDaemon() {
+	h.Conf.Tiempo = 100 * time.Second
+	fmt.Println("DAEMON")
+}
+func (c *Config) init() {
+	var tick = flag.Duration("tick", 1*time.Second, "Ticking interval")
+	c.Tiempo = *tick
+}
+func run(con context.Context, c *MyHandler, stdout io.Writer) error {
+	c.Conf.init()
+	log.SetOutput(os.Stdout)
+	for {
+		select {
+		case <-con.Done():
+			return nil
+		case <-time.Tick(c.Conf.Tiempo):
+			c.StartDaemon()
+		}
+	}
 }
