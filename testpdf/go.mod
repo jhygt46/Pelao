d1d17832e437e4b915a5db2ffa7a1d4d512ea044
jhygt46 <misitiodelivery@gmail.com>
2022-12-21 15:35:50 -0300

    aa

---
+++ testpdf/go.mod
@@ -0,0 +1,21 @@
+module prod
+
+go 1.18
+
+require (
+	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.7.2 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/boombuler/barcode v1.0.1 // indirect
+	github.com/dgrr/http2 v0.3.5 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/johnfercher/maroto v0.39.0 // indirect
+	github.com/jung-kurt/gofpdf v1.16.2 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/mithorium/secure-fasthttp v0.0.0-20161207145432-6499985a395f // indirect
+	github.com/ruudk/golang-pdf417 v0.0.0-20181029194003-1af4ab5afa58 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.37.0 // indirect
+	github.com/valyala/fastrand v1.0.0 // indirect
+)
---
+++ testssl/prod.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
+
+	"github.com/valyala/fasthttp"
+)
+
+type Config struct {
+	Tiempo time.Duration `json:"Tiempo"`
+}
+type MyHandler struct {
+	Conf  Config `json:"Conf"`
+	Count uint64 `json:"Count"`
+}
+
+func main() {
+
+	var port string
+	if runtime.GOOS == "windows" {
+		port = ":81"
+	} else {
+		port = ":8080"
+	}
+
+	pass := &MyHandler{
+		Conf: Config{},
+	}
+
+	con := context.Background()
+	con, cancel := context.WithCancel(con)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGHUP)
+	defer func() {
+		signal.Stop(signalChan)
+		cancel()
+	}()
+	go func() {
+		for {
+			select {
+			case s := <-signalChan:
+				switch s {
+				case syscall.SIGHUP:
+					pass.Conf.init()
+				case os.Interrupt:
+					cancel()
+					os.Exit(1)
+				}
+			case <-con.Done():
+				log.Printf("Done.")
+				os.Exit(1)
+			}
+		}
+	}()
+	go func() {
+		fasthttp.ListenAndServe(port, pass.HandleFastHTTP)
+	}()
+	if err := run(con, pass, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+
+}
+
+func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
+
+	if string(ctx.Method()) == "GET" {
+		switch string(ctx.Path()) {
+		case "/":
+			h.Count++
+			fmt.Fprintf(ctx, "HOLA")
+		case "/stats":
+			fmt.Fprintf(ctx, "Count %v", h.Count)
+		default:
+			ctx.Error("Not Found", fasthttp.StatusNotFound)
+		}
+	}
+
+}
+
+// DAEMON //
+func (h *MyHandler) StartDaemon() {
+	h.Conf.Tiempo = 100 * time.Second
+	fmt.Println("DAEMON")
+}
+func (c *Config) init() {
+	var tick = flag.Duration("tick", 1*time.Second, "Ticking interval")
+	c.Tiempo = *tick
+}
+func run(con context.Context, c *MyHandler, stdout io.Writer) error {
+	c.Conf.init()
+	log.SetOutput(os.Stdout)
+	for {
+		select {
+		case <-con.Done():
+			return nil
+		case <-time.Tick(c.Conf.Tiempo):
+			c.StartDaemon()
+		}
+	}
+}
