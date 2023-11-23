affe1f8455654cf0965f9ac4cb35eb570a0930e6
jhygt46 <misitiodelivery@gmail.com>
2023-11-23 18:56:54 -0300

    aa

--- DAEMON/prod.go
+++ DAEMON/prod.go
@@ -53,7 +53,7 @@ func main() {
 		fmt.Println("Ok ... Archivo de Configuracion leido correctamente")
 		if err := json.Unmarshal(passwords, &pass.Passwords); err == nil {
 			fmt.Println("Ok ... Unmarshal datos de configuracion")
-			fmt.Println(pass.Passwords)
+			fmt.Println(time.Since(pass.Passwords.FechaCert).Seconds())
 			if pass.Request() {
 				fmt.Println("Ok ... Servicio Arriba, Reiniciando ...")
 			} else {
